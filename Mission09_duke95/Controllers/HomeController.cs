4dfebdcff3d0d4fb49c6e390abde7aa7f2c14ff8
DukeofNeptune <dallin.duke4@gmail.com>
2023-03-01 19:20:12 -0700

    Add project files.

---
+++ Mission09_duke95/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission09_duke95.Models;
+using Mission09_duke95.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_duke95.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private IBookstoreRepository repo;
+
+        public HomeController(IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        //Set up Home controller pagination part
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumProjects = repo.Books.Count(),
+                    ProjectsPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+
+       
+    }
+}
